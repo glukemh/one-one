0a00e5f7bce7b8802be78892d5c4f99830e42d62
Luke Mendelman-Haenn <glukemh@gmail.com>
2022-09-10 21:54:53 -0500

    homepage content

---
+++ components/home-content.tsx
@@ -0,0 +1,11 @@
+const HomeContent = () => {
+  return (
+    <main>
+      <section aria-label="Blog Posts">
+        <div>Under Construction</div>
+      </section>
+    </main>
+  )
+}
+
+export default HomeContent
