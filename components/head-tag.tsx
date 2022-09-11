a59794cd2f9718d421bda42aea184744dd103c9a
Luke Mendelman-Haenn <glukemh@gmail.com>
2022-09-10 21:46:24 -0500

    head tag and linting

---
+++ components/head-tag.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const HeadTag = () => {
+  return (
+    <Head>
+      <title>1 to 1</title>
+      <meta charSet="utf-8" />
+      <meta name="description" content="A blog about math and programing" />
+      <meta name="keywords" content="Math, Code, Programming" />
+      <meta name="author" content="Luke Mendelman-Haenn" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
+
+export default HeadTag
